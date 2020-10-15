a4b83c82ab88d8b32ec71bae87a22151c016e1ad
jespermeller <jesper.meller@hotmail.com>
2020-10-15 13:24:40 +0200

    stadig kun ælndret model laget

--- Backend/NordicBio.model/Movie.cs
+++ Backend/NordicBio.model/Movie.cs
@@ -6,8 +6,8 @@ namespace NordicBio.model
     {
         public int Id { get; set; }
         public string Title { get; set; }
-        public string Release_Date { get; set; }
-        public double Vote_Average { get; set; }
-        public string Poster_Path { get; set; }
+        public string ReleaseDate { get; set; }
+        public double VoteAverage { get; set; }
+        public string PosterPath { get; set; }
     }
 }
