897eb058f8c8343027a94db62591a810987af5c9
kris914g <kristoffer.aagard@gmail.com>
2020-11-03 12:51:38 +0100

    Initial commit for book movie

--- frontend/src/router/routes.js
+++ frontend/src/router/routes.js
@@ -2,6 +2,7 @@
 // Routes to redirect user
 const frontpage = () => import('../layouts/frontpage.vue')
 const loginAuth = () => import('../layouts/LoginAuth.vue')
+const singleMovie = () => import('../components/singleMovie.vue')
 
 const routes = [
       {
@@ -13,7 +14,12 @@ const routes = [
          path: '/login',
          name: 'login',
          component: loginAuth,
-      }
+      },
+    {
+        path: '/movie/:id',
+        name: 'singleMovie',
+        component: singleMovie
+    }
    ] 
 
 export default routes
