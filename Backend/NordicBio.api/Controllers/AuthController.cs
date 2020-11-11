1620af8cb5fab380f18424d2f0cebf043507357d
Kasper Nielsen <Nielsen1994@hotmail.com>
2020-11-11 09:50:33 +0100

    k

--- Backend/NordicBio.api/Controllers/AuthController.cs
+++ Backend/NordicBio.api/Controllers/AuthController.cs
@@ -46,11 +46,6 @@ namespace NordicBio.api.Controllers
             {
                 return NotFound("User not found or wrong crenditials!");
             }
-
-            Token t = new Token();
-            t.Key = GenerateJSONWebToken();
-            var tokenString = JsonConvert.SerializeObject(t, Formatting.Indented);
-            return Ok(tokenString);
         }
 
         [HttpPost]
