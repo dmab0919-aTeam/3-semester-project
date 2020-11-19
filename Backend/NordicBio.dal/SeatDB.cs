8faee55efe6d0b9b51a14b983b125f9d67214faa
Kasper Nielsen <Nielsen1994@hotmail.com>
2020-11-19 11:34:36 +0100

    Seat api thing

---
+++ Backend/NordicBio.dal/SeatDB.cs
@@ -0,0 +1,40 @@
+﻿using Dapper;
+using NordicBio.model;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+
+namespace NordicBio.dal
+{
+    public class SeatDB
+    {
+        private string _constring;
+
+        public SeatDB(string constring)
+        {
+            this._constring = constring;
+        }
+
+        public IEnumerable<Seat> GetAllSeats(int showingID)
+        {
+            var parameters = new { ShoginID = showingID };
+            string sql = "SELECT * FROM Seats WHERE ShowingID = @ShowingID";
+            List<Seat> res;
+
+            using (var connection = new SqlConnection(_constring))
+            {
+                try
+                {
+                    res = connection.Query<Seat>(sql).ToList();
+                }
+                catch (Exception)
+                {
+                    throw;
+                }
+            }
+
+            return res;
+        }
+    }
+}
