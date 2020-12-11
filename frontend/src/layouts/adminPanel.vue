7f6d6cf21ef223665c818733e2e73410a6aa5b0a
Kasper Nielsen <Nielsen1994@hotmail.com>
2020-12-11 13:16:31 +0100

    sdfsd

--- frontend/src/layouts/adminPanel.vue
+++ frontend/src/layouts/adminPanel.vue
@@ -8,20 +8,19 @@
             <div class="showing-form">
                 <form class="form" @submit.prevent="createshowing">
                 <h3>Create Showing</h3>
-                <label>Price:</label>
-                <br>
+                <label>Price:</label><br>
                 <input id="price" v-model="firstname" type="number" placeholder="Enter Price"><br>
-                <label>Date:</label>
-                <br>
+                
+                <label>Date:</label><br>
                 <input id="date" v-model="lastname" type="date" placeholder=""><br>
-                <label>Time:</label>
-                <br>
+                
+                <label>Time:</label><br>
                 <input id="time" v-model="regemail" type="time" placeholder=""><br>
-                <label>Hallnumber:</label>
-                <br>
+                
+                <label>Hallnumber:</label><br>
                 <input id="hallnumber" v-model="phone" type="number" placeholder="Enter Number"><br>
-                <label>Movie Id:</label>
-                <br>
+                
+                <label>Movie Id:</label><br>
                 <input id="movieid" v-model="regpassword" type="number" placeholder="Enter valid id"><br>
                 <button type="submit">Create</button><br>
                 </form>
@@ -29,32 +28,28 @@
              <div class="list-container">
                 <h3>Showings:</h3>
                 <ul class="showing-list">
-                    <li class="list-item">insert showing here</li>
-                    <li class="list-item">insert showing here</li>
-                    <li class="list-item">insert showing here</li>
-                    <li class="list-item">insert showing here</li>
-                    <li class="list-item">insert showing here</li>
-                    <li class="list-item">insert showing here</li>
+                    <li class="list-item">insert showing here 
+                        <button class="list-button">Update</button>
+                        <button class="list-button">Delete</button>
+                    </li>   
                 </ul>
             </div>
-
             <div class="user-form">
                 <form class="form" @submit.prevent="createuser">
                 <h3>Create User</h3>
-                <label>First name:</label>
-                <br>
+                <label>First name:</label><br>
                 <input id="firstname" v-model="firstname" type="text" placeholder="Enter First Name"><br>
-                <label>Last name:</label>
-                <br>
+                
+                <label>Last name:</label><br>
                 <input id="lastname" v-model="lastname" type="text" placeholder="Enter Last Name"><br>
-                <label>Email:</label>
-                <br>
+                
+                <label>Email:</label><br>
                 <input id="email" v-model="regemail" type="email" placeholder="Enter Email"><br>
-                <label>Phone number:</label>
-                <br>
+                
+                <label>Phone number:</label><br>
                 <input id="phonenumber" v-model="phone" type="text" placeholder="Enter Phone Number"><br>
-                <label>Password:</label>
-                <br>
+                
+                <label>Password:</label><br>
                 <input id="password" v-model="regpassword" type="password" placeholder="Enter an password"><br>
                 <button type="submit">Create</button>
                 </form>
@@ -62,16 +57,12 @@
             <div class="list-container">
                 <h3>Users:</h3>
                 <ul class="user-list">
-                    <li class="list-item">insert showing here</li>
-                    <li class="list-item">insert showing here</li>
-                    <li class="list-item">insert showing here</li>
-                    <li class="list-item">insert showing here</li>
-                    <li class="list-item">insert showing here</li>
-                    <li class="list-item">insert showing here</li>
+                    <li class="list-item">insert showing here
+                        <button class="list-button">Update</button>
+                        <button class="list-button">Delete</button>
+                    </li>
                 </ul>
             </div>
-
-
         </div>
     </div>
 </template>
@@ -101,9 +92,17 @@ html, body {
 input {
     margin-bottom: 1em;
 }
+
+.list-button {
+    float: right;
+    padding-left: 2em;
+    padding-right: 2em;
+    margin-right: .5em;
+    cursor: pointer;
+}
 .container {
     background-color: white;
-    overflow: hidden;
+    overflow: scroll;
     border-radius: 1em;
     height: 100vh;
     padding: 1em;
@@ -120,29 +119,40 @@ input {
     width: 100%;
     height: 100%;
 }
-.showing-form, .user-form, .list-container {
+.showing-form, .user-form {
     float: left;
     margin: 1%;
     padding: .5em;
-    border: 2px solid black;
+    border: 2px solid grey;
     border-radius: 5px;
     height: 60%;
     overflow: scroll;
     width: 20%;
 }
 
+.list-container {
+    float: left;
+    height: 60%;
+    overflow: scroll;
+    margin: 1%;
+    padding: .5em;
+    width: 70%;
+    border: 2px solid grey;
+}
+
 .showing-list, .user-list{
   margin:0;
   overflow:auto;
-  padding:0;
+  padding: .5em;
   text-indent:10px;
   max-width: 100%;
-  max-height: 250px;
-  height: 250px;
+  max-height: 340px;
+  height: 340px;
   border: 1px solid black;
 }
 .list-item{
   line-height:25px;
+  border-bottom: 1px solid;
 }
 
 li:nth-child(even){
